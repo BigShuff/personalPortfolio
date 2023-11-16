db090b09d4243a30872ea71de78c6302cfd2f1e6
BigShuff <stevedavis0504@gmail.com>
2023-11-16 17:18:55 +0000

    first commit

---
+++ resources/js/script.js
@@ -0,0 +1,10 @@
+//grab my flyer div
+const rollImage1 = document.getElementsByClassName('image1');
+
+//add an event listener to my div
+rollImage1.addEventListener('mouseenter', function(){
+    //oposite is hidden
+    flyer.style.visibility = 'visible';
+});
+
+document.querySelector('.sectionNine').innerHTML = "Get Fucked";
