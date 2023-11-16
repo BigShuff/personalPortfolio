e008056b228f90805783d3f8fba41e218225ee98
BigShuff <stevedavis0504@gmail.com>
2023-11-16 18:02:38 +0000

    end of day 16-Nov-23

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,10 +1,20 @@
+const flyer = document.querySelector('.flyer')
+const flyerImg = document.querySelector('.flyerImg')
+flyer.style.visibility = 'hidden';
 //grab my flyer div
-const rollImage1 = document.getElementsByClassName('image1');
+const rollImage1 = document.querySelector('.image1');
 
 //add an event listener to my div
 rollImage1.addEventListener('mouseenter', function(){
     //oposite is hidden
     flyer.style.visibility = 'visible';
+    flyerImg.setAttribute('src', 'resources/images/southfields.png')
+    
 });
 
-document.querySelector('.sectionNine').innerHTML = "Get Fucked";
+//add an event listener to my div
+rollImage1.addEventListener('mouseleave', function(){
+    //oposite is hidden
+    flyer.style.visibility = 'hidden';
+    //unload image
+});
